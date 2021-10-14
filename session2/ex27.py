da9eb8c8e16403bab9c3e136477045e5afa2e45c
StefaniaCIT <stefaniachiriac04@yahoo.com>
2021-10-14 20:34:45 +0300

    rez ex27

--- session2/ex27.py
+++ session2/ex27.py
@@ -7,3 +7,11 @@
         Veti primi: 5
         Veti printa: 'ashdj' (poate fi orice alt string)
 """
+import string
+import random
+a = int(input())
+l = []
+for i in range(a):
+    b= random.choice(string.ascii_lowercase)
+    l.append(b)
+print (''.join(l))
