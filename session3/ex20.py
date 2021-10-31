05f1e468413c12e2f400997e1198d98df2136763
StefaniaCIT <stefaniachiriac04@yahoo.com>
2021-10-31 21:14:58 +0200

    incercare ex20

--- session3/ex20.py
+++ session3/ex20.py
@@ -5,3 +5,15 @@
     Toate astea le veti face intr-o functie read_from_file(file), unde
     file este numele fisierului primit dat ca parametru.
 """
+import json
+
+def read_from_file(file):
+    with open('ceva.json', "r") as f:
+        data = json.load(f)
+        print(data)
+
+read_from_file('ceva')
+
+    
+
+
