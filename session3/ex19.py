f743b636c97da0dc8c373bf15f494f29a553fd6a
StefaniaCIT <stefaniachiriac04@yahoo.com>
2021-10-31 20:35:03 +0200

    incercare ex19

--- session3/ex19.py
+++ session3/ex19.py
@@ -16,3 +16,38 @@
             }
 
 """
+import json
+import random
+
+def f(x):
+    x = 'ceva'
+    return x 
+def writeTOJSONFile(filename, data):
+    filePathNameWExt = filename + ".json"
+    jsonString = json.dumps(data)
+    jsonFile = open(filePathNameWExt, "a")
+    jsonFile.write(jsonString)
+    jsonFile.close()
+
+def myValue():
+    letters_nr = "qwertyuiopasdfghjklzxcvbnm0123456789"
+    nr = [3, 4, 5, 6]
+    nr = random.choice(nr)
+    value = ''.join(random.choice(letters_nr) for i in range(nr))
+    return value
+
+def myKey():
+    numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    key = random.choice(numbers)
+    return key 
+
+filename = f("")
+
+data = {
+    myKey() : myValue(),
+    myKey() : myValue(),
+    myKey() : myValue(),
+    myKey() : myValue(),
+}
+
+writeTOJSONFile(filename, data)
